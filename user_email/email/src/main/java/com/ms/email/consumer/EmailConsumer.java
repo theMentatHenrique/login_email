cfbc03793075353f79d1755187f3cfba9d5a4ab9
mentatHenrique <henrique.paim01@edu.pucrs.br>
2023-11-04 23:07:14 -0300

    ms-user concluído

--- user_email/email/src/main/java/com/ms/email/consumer/EmailConsumer.java
+++ user_email/email/src/main/java/com/ms/email/consumer/EmailConsumer.java
@@ -1,5 +1,6 @@
 package com.ms.email.consumer;
 
+import com.ms.email.dtos.EmailRecordDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -8,8 +9,8 @@ import org.springframework.stereotype.Component;
 public class EmailConsumer {
 
     @RabbitListener(queues = "${broker.queue.email.name}")
-    public void listenEmailQueue(@Payload String string) {
-        System.out.println(string);
+    public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto) {
+        System.out.println(emailRecordDto.emailTo());
     }
 
 }
--- user_email/user/src/main/java/com/ms/user/services/UserService.java
+++ user_email/user/src/main/java/com/ms/user/services/UserService.java
@@ -1,6 +1,7 @@
 package com.ms.user.services;
 
 import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducer;
 import com.ms.user.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +13,14 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private UserProducer userProducer;
+
     @Transactional
     public UserModel save(UserModel userModel) {
-        return userRepository.save(userModel);
+        UserModel save = userRepository.save(userModel);
+        userProducer.publishMessageMail(userModel);
+        return userModel;
     }
 
 }
